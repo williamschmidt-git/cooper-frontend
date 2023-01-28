8d97a38bab8b275baefb61d13e53c52dc3483ecf
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-28 13:14:57 -0300

    first commit

---
+++ src/context/providers/Provider.jsx
@@ -0,0 +1,17 @@
+import React, { useMemo, useState } from 'react';
+import Context from '../Context';
+
+function Provider({ children }) {
+  const [teste, setTeste] = useState('');
+  const value = useMemo(() => ({
+    teste, setTeste,
+  }), [setTeste]);
+
+  return (
+    <Context.Provider value={value}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+export default Provider;
