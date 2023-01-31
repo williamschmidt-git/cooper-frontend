61965b4dded2c6f56da2d86e8077fdc9d3994325
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-31 19:09:38 -0300

    feat: refactoring files

---
+++ src/pages/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <>
+      <header>
+        <div className="topo">
+          <div className="logo">
+            <img src="./svgs/Fill 1.svg" alt="less than symbol" className="logo-cooper" />
+            <h1 className="logo-title">coopers</h1>
+          </div>
+          <button type="button" className="login-button">entrar</button>
+        </div>
+      </header>
+      <div />
+    </>
+  );
+}
