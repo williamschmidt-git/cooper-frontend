ad05af88bbb5e3375116291c4d9b747fcfc61109
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-31 19:36:59 -0300

    title component created

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -13,7 +13,16 @@ export default function Main() {
           <button type="button" className="login-button">entrar</button>
         </div>
       </header>
-      <div />
+
+      <div className="Title">
+        <div>
+          <p className="title-first-text">Organize</p>
+          <p className="title-second-text">your daily jobs</p>
+        </div>
+
+        <p className="title-third-text">The only way to get things done</p>
+        <button type="button" className="title-button">Go to To-do list</button>
+      </div>
     </>
   );
 }
