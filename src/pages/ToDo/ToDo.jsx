e7ddbb4b6c21a518cec5de339c2baaff25533524
williamschmidt-git <9williamschmidt@gmail.com>
2023-02-01 00:05:34 -0300

    todo list text

--- src/pages/ToDo/ToDo.jsx
+++ src/pages/ToDo/ToDo.jsx
@@ -3,8 +3,11 @@ import './index.css';
 
 export default function ToDo() {
   return (
-    <div className="todo-title">
-      todo
+    <div className="todo-box">
+      <h2 className="todo-title">To-do List</h2>
+      <p className="todo-text">
+        Drag and drop to set your main priorities, check when done and create what´s new.
+      </p>
     </div>
   );
 }
