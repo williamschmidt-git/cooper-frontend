a0d4914da9e6fa2f034ca769aab26e89769a6ace
williamschmidt-git <9williamschmidt@gmail.com>
2023-02-03 18:38:22 -0300

    mobile css style done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import './index.css';
 
 export default function Form() {
   return (
-    <div>
-      form
-    </div>
+    <form className="form-contact">
+      <label htmlFor="name">
+        Your name
+        <input type="text" placeholder="type your name here" id="name" />
+      </label>
+      <label htmlFor="email">
+        E-mail*
+        <input type="text" placeholder="example@example.com" id="email" />
+      </label>
+
+      <label htmlFor="telephone">
+        Telephone*
+        <input type="text" placeholder="( ) _____-____" id="telephone" />
+      </label>
+
+      <label htmlFor="message">
+        Message*
+        <textarea id="message" placeholder="type what you want to say to us" className="message-input" />
+      </label>
+
+      <button className="send-message-button" type="submit">SEND NOW</button>
+    </form>
   );
 }
