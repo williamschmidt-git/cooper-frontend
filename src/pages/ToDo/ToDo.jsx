5d2cdcc421a4977df03f2579314b36ff1982f315
williamschmidt-git <9williamschmidt@gmail.com>
2023-02-04 17:53:50 -0300

    changes

--- src/pages/ToDo/ToDo.jsx
+++ src/pages/ToDo/ToDo.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import './index.css';
 
 import ToDoTitle from '../../components/ToDoTitle/ToDoTitle';
 import DragAndDrop from '../../components/DragAndDrop/DragAndDrop';
